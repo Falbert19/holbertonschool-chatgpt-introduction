9a4ce8491c32731c2367714e68f7ace2e9743e52
Falbert19 <fernandoalbert19@gmail.com>
2025-01-19 23:29:38 -0400

    print

--- debugging/print_arguments.py
+++ debugging/print_arguments.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
 
-for i in range(1, len(sys.arg)):
-    print(sys.argv[i])
+if __name__ == "__main__":
+    for i in range(1, len(sys.argv)):
+        print(sys.argv[i])
